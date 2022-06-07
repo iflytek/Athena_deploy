58bee7e57df506c38fbc42cfd523fcedb933c375
ybyang7 <ybyang7@iflytek.com>
2022-06-07 15:19:18 +0000

    add atoms  loadalance wbgate  mmorc_ase

---
+++ chart/mmocr_ase/demo.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import base64
+
+image = open("demo_text_recog.jpg","rb")
+img = base64.b64encode(image.read())
+
+
+
+url = "http://10.242.99.121:8889/mmocr"
+method = "POST"
+headers = {"Content-Type":"application/json"}
+data = {
+    "header": {
+        "app_id": "123456",
+        "uid": "39769795890",
+        "did": "SR082321940000200",
+        "imei": "8664020318693660",
+        "imsi": "4600264952729100",
+        "mac": "6c:92:bf:65:c6:14",
+        "net_type": "wifi",
+        "net_isp": "CMCC",
+        "status": 3,
+        "request_id": None
+    },
+    "parameter": {
+        "mmocr": {
+            "category": "en_phfw_chapter",
+            "application_mode": "common_gpu",
+            "gpu_id": "first",
+            "gpu_type": "T4G16",
+            "boxes": {
+                "encoding": "utf8",
+                "compress": "raw",
+                "format": "json"
+            }
+        }
+    },
+    "payload": {
+        "data": {
+            "encoding": "jpg",
+            "image": img.decode("utf-8"),
+            "status": 3
+        }
+    }
+}
+
+resp = requests.post(url,headers=headers,data=json.dumps(data))
+
+print(resp.status_code)
+
+print(resp.content)
